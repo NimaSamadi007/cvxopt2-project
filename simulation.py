4f74f5ee81593ddbfd85154047666a9bf568278b
Nima Samadi <nbr.smd@gmail.com>
2023-02-07 00:53:57 +0330

    Fix simulation bugs and problem formulation

--- simulation.py
+++ simulation.py
@@ -1,62 +1,100 @@
 import cvxpy as cp
 import numpy as np
 
-np.random.seed(0)
+m = 2
+n = 3
+K = 2
 
-m = 10
-n = 6
+## parameters
+C = np.array([[100, 200, 200],
+              [100, 200, 200],
+              [100, 200, 200]]).reshape((n, m+1))
+R = np.array([[10, 20, 30],
+              [12, 18, 16]]).reshape((n-1, m+1))
+m_l = np.array([400, 250, 300]).reshape((n, 1))
+m_d = np.array([500, 500, 420]).reshape((m+1, 1))
+alpha = np.array([50, 100, 100]).reshape((m+1, 1))
 
-# parameters
-c = np.random.randint(20, size=(n, m+1))
+## middle variables
+p = np.zeros(( (m+1)*(2*n-1), 1) )
+p[:(m+1)*n, :] = (C / alpha.reshape(1, -1)).T.reshape(-1, 1)
 
-alpha = np.random.randint(1, 20, size=(m+1, 1))
+q = 0.5 * R.T.reshape(-1, 1)
 
-R = np.random.randint(20, size=(n-1, m+1))
+S_1 = np.zeros( ((m+1)*(n-1), (m+1)*(2*n-1)) )
+for idx in range(m+1):
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range(idx+idx*(n-1), idx+(idx+1)*(n-1)))
+  S_1[temp_idx_x, temp_idx_y] = -np.ones(n-1)
+  
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range(1+idx+idx*(n-1), 1+idx+(idx+1)*(n-1)))
+  S_1[temp_idx_x, temp_idx_y] = np.ones(n-1)
 
-m_in = np.random.randint(20, size=(n, 1))
-m_devices = np.random.randint(20, size=(m+1, 1))
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range((m+1)*(n-1)+1+idx+idx*(n-1), (m+1)*(n-1)+1+idx+(idx+1)*(n-1)))
+  S_1[temp_idx_x, temp_idx_y] = -np.ones(n-1)
 
-p = np.zeros(((2*n-1)*(m+1), 1))
-p[:(m+1)*n, :] = (c / alpha.reshape(1, -1)).T.reshape(-1, 1)
+S_2 = np.zeros( ((m+1)*(n-1), (m+1)*(2*n-1)) )
+for idx in range(m+1):
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range(idx+idx*(n-1), idx+(idx+1)*(n-1)))
+  S_2[temp_idx_x, temp_idx_y] = -np.ones(n-1)
+  
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range(1+idx+idx*(n-1), 1+idx+(idx+1)*(n-1)))
+  S_2[temp_idx_x, temp_idx_y] = np.ones(n-1)
 
-qz = R.T.reshape(-1, 1)
+  temp_idx_x = np.array(range(idx*(n-1), (idx+1)*(n-1)))
+  temp_idx_y = np.array(range((m+1)*(n-1)+1+idx+idx*(n-1), (m+1)*(n-1)+1+idx+(idx+1)*(n-1)))
+  S_2[temp_idx_x, temp_idx_y] = np.ones(n-1)
 
-S = -np.eye( (m+1)*(n-1), (m+1)*(2*n-1) ) + np.eye( (m+1)*(n-1), (m+1)*(2*n-1), k=1 ) - np.eye( (m+1)*(n-1), (m+1)*(2*n-1), k=(m+1)*n )
-
-K = 3
 h = K * alpha
 
 L = np.zeros( (m+1, (m+1)*(2*n-1)) )
 for i in range(m):
-  L[i, i*n:(i+1)*n] = c[:, i]
+  L[i, i*n:(i+1)*n] = C[:, i]
 
-I = np.zeros((m+1)*(n-1))
-y = np.ones( ((m+1)*(2*n-1), 1) )
-y[-(m+1)*(n-1):, 0] = 0
+I = np.ones((n, 1))
+
+E = np.zeros( (n, (m+1)*(2*n-1)) )
+for i in range(n):
+  idx = list(range(i, (m+1)*(n)+i, n))
+  E[i, idx] = 1
+
 M = np.zeros( (m+1, (m+1)*(2*n-1)) )
-for i in range(m):
-  M[i, i*n:(i+1)*n] = m_in.reshape(1, -1)
+for i in range(m+1):
+  M[i, i*n:(i+1)*n] = m_l.reshape(1, -1)
+# print(M)
+# print(m_d)
+V = np.zeros( ((m+1)*(n-1), (m+1)*(2*n-1)) )
+V[:(m+1)*(n-1), 1:(m+1)*(n-1)+1] = np.diag( -np.ones((m+1)*(n-1)) )
+
+U = np.zeros( ((m+1)*(n-1), (m+1)*(2*n-1)) )
+U[-(m+1)*(n-1):, -(m+1)*(n-1):] = np.diag( -np.ones((m+1)*(n-1)) )
 
 # optimization variable
 z = cp.Variable( ( (2*n-1)*(m+1), 1), boolean=True)
-zz = cp.Variable( (m+1)*(n-1), boolean=True)
+w = cp.Variable( ((m+1)*(n-1), 1), boolean=True)
 
-# print(np.linalg.eigvals(Q))
-# print(np.all(np.linalg.eigvals(Q) > 0))
-
-# print(M.shape)
-# print(z.shape)
-# print(m_devices.shape)
-
-objective = cp.Minimize(p.T @ z + 0.5 * qz.T @ zz)
-constrinats = [S @ z <= 0,
+objective = cp.Minimize(p.T @ z + q.T @ w)
+constraints = [S_1 @ z <= 0,
+               -S_2 @ z <= 0,
                L @ z <= h,
-               y.T @ z == I,
-               M @ z <= m_devices]
+               E @ z == I,
+               M @ z <= m_d,
+               U @ z + w <= 0,
+               V @ z + w <= 0]
 
-prob = cp.Problem(objective, constrinats)
+prob = cp.Problem(objective, constraints)
 prob.solve(solver='MOSEK')
 
 print("Status:", prob.status)
 print("Optimal value", prob.value)
 # print("Optimal var", z.value)
+
+x = np.array(z.value)[:(m+1)*n].reshape((n, m+1))
+print(f"Optimal variable:\n{x}")
+print(z.value.reshape(1, -1))
+print(w.value.reshape(1, -1))
+print(q.T)
